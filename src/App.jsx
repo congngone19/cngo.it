4b368e9b89041af017f90840bf2e57f635a85592
NgoNC <nguyencongngoo190802@gmail.com>
2025-05-12 21:58:13 +0700

    update router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from "react-router-dom";
 import Login from './component/login';
 import Nav from './component/nav'
 import Signup from './component/signup';
@@ -7,14 +7,14 @@ import Contact from './component/contact';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   )
 }
 
